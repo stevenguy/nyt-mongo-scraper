384c20f74c915c21dd699014060e163e56c5d345
Steve N <nguy.steve@gmail.com>
2018-12-08 01:09:02 -0800

    still need save article, delete save,  populate save, and add note

---
+++ public/js/app.js
@@ -0,0 +1,63 @@
+// dependencies
+const homeButton = document.getElementById('homeButton')
+const saveArticles = document.getElementById('saveArticles')
+const scrapeAll = document.getElementById('scrapeAll')
+const deleteAll = document.getElementById('deleteAll')
+const deleteSave = document.getElementByClassName('save')
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    $.ajax({
+        url: "/scrape",
+        method: "GET",
+    })
+    console.log("scraping done")
+});
+
+scrapeAll.addEventListener('click', function() {
+    $.getJSON("/all", function(data) {
+        console.log(data);
+        for (let i = 0; i < data.length; i++) {
+            // Send a "Scrape Complete" message to the browser
+            const cardDiv = $('<div id="' + data[i]._id + '" class="uk-card uk-card-default uk-width-1-2@m">')
+            const cardHeader = $('<div class="uk-card-header">')
+            const cardTitle = $('<h3 class="uk-card-title uk-margin-remove-bottom">' + data[i].title + '</h3>')
+            const cardBody = $('<div class="uk-card-body">')
+            const cardSummary = $('<p>' + data[i].summary + '</p>')
+            const cardFooter = $('<div class="uk-card-footer">')
+            const cardAction = $('<a class="uk-button uk-button-default" href="' + data[i].link + '">Read more</a>')
+            const save = $('<a class="uk-button uk-button-default save">Save</a>')
+            cardFooter.append(cardAction)
+            cardFooter.append(save)
+            cardBody.append(cardSummary)
+            cardHeader.append(cardTitle)
+            cardDiv.append(cardHeader)
+            cardDiv.append(cardBody)
+            cardDiv.append(cardFooter)
+            // Push API call to html
+            $('#scrapeOutput').append(cardDiv)
+        }
+    })
+})
+
+deleteAll.addEventListener('click', function() {
+
+    $.getJSON("/clearall", function(data) {
+        console.log(data)
+    })
+    $('uk-card').remove();
+    location.reload() 
+})
+
+saveArticles.addEventListener('click', function() {
+
+    $.getJSON("/update/" + _id, function(data) {
+        console.log(data)
+    })
+    $('#scrapeOutput').hide();
+    $('#saveOutput').show();
+})
+
+homeButton.addEventListener('click', function() {
+    $('#scrapeOutput').show();
+    $('#saveOutput').hide();
+})
---
+++ server.js
@@ -0,0 +1,163 @@
+// Dependencies
+const express = require('express')
+const mongojs = require('mongojs')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const bodyparser = require('body-parser')
+const path = require("path");
+
+// Initialize Express
+const app = express()
+
+const PORT = process.env.PORT || 8080;
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Database configuration
+const databaseUrl = 'scraper'
+const collections = ['scrapedData', 'saveData']
+
+// Hook mongojs configuration to the db constiable
+const db = mongojs(databaseUrl, collections)
+
+db.on('error', function (error) {
+  console.log('Database Error:', error)
+})
+
+// Main route 
+app.get('/', function (req, res) {
+    res.render('/')
+})
+
+
+// Routes needed
+// 1. Retrieved all scraped data from DB
+// 2. Scrape all route
+// 3. Delete Save Route
+// 4. Delete ALL
+// 5. Save Article
+
+// Retrieve data from the db
+app.get('/all', function (req, res) {
+// Find all results from the scrapedData collection in the db
+    db.scrapedData.find({}, function (error, found) {
+        // Throw any errors to the console
+        if (error) {
+        console.log(error)
+        }
+        // If there are no errors, send the data to the browser as json
+        else {
+        res.json(found)
+        }
+    })
+})
+
+// First, tell the console what server.js is doing
+console.log("\n***********************************\n" +
+            "from NYT's webdev board:" +
+            "\n***********************************\n");
+
+app.get('/scrape', function (req, res) {
+
+    axios.get("https://www.nytimes.com/section/your-money").then(function(response) {
+
+        const $ = cheerio.load(response.data);
+        const results = [];
+
+        $("div.css-4jyr1y").each(function(i, element) {
+            
+            const title = $(element).children('a').children('h2').text()
+            const link = $(element).children().attr("href");
+            const summary = $(element).children('a').children('p').text()
+
+            results.push({
+                title: title,
+                link: "https://www.nytimes.com" + link,
+                summary: summary,
+                save: 'no'
+            });
+            db.scrapedData.insert({
+                title: title,
+                link: "https://www.nytimes.com" + link,
+                summary: summary,
+                save: 'no'
+            })
+        });
+        console.log(results);
+        // Send a "Scrape Complete" message to the browser
+        res.send('Scrape Complete')
+    })
+})
+
+app.get("/clearall", function(req, res) {
+    // Remove every note from the notes collection
+    db.scrapedData.remove({}, function(error, response) {
+      // Log any errors to the console
+        if (error) {
+            console.log(error);
+            res.send(error);
+        }
+        else {
+            // Otherwise, send the mongojs response to the browser
+            // This will fire off the success function of the ajax request
+            console.log(response);
+            res.send(response);
+        }
+    });
+});
+
+app.post("/update/:id", function(req, res) {
+
+    db.saveData.insert(
+        {
+            title: title,
+            link: link,
+            summary: summary,
+            save: "yes"
+        },
+        function(error, edited) {
+            // Log any errors from mongojs
+            if (error) {
+            console.log(error);
+            res.send(error);
+            }
+            else {
+            // Otherwise, send the mongojs response to the browser
+            // This will fire off the success function of the ajax request
+            console.log(edited);
+            res.send(edited);
+            }
+        }
+    );
+});
+
+app.get("/delete/:id", function(req, res) {
+    // Remove a note using the objectID
+    db.saveData.remove(
+        {
+            _id: mongojs.ObjectID(req.params.id)
+        },
+        function(error, removed) {
+            // Log any errors from mongojs
+            if (error) {
+            console.log(error);
+            res.send(error);
+            }
+            else {
+            // Otherwise, send the mongojs response to the browser
+            // This will fire off the success function of the ajax request
+            console.log(removed);
+            res.send(removed);
+            }
+        }
+    );
+});
+
+// Listen on port 3000
+app.listen(3000, function () {
+    console.log('App running on port 3000!')
+})
